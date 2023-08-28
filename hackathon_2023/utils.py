bef91a01d5c227f8493a2146df14d777b13efc4b
Bryce York <bryce@bryceyork.com>
2023-08-28 18:35:07 -0400

    feat: exclude the bot's own messages from the summary corpus

--- hackathon_2023/utils.py
+++ hackathon_2023/utils.py
@@ -5,8 +5,25 @@ from slack_sdk.errors import SlackApiError
 async def get_channel_history(client: WebClient, channel_id: str) -> list:
     try:
         response = client.conversations_history(channel=channel_id, limit=1000)  # 1000 is the max limit
-        # todo: remove TLDR bot messages
-        return response["messages"]
+        bot_id = await get_bot_id(client)
+        # todo: exclude messages that start with `/` (i.e. slash commands)
+        # todo: support excluding other bots too
+        return [msg for msg in response["messages"] if msg.get("bot_id") != bot_id]
     except SlackApiError as e:
         print(f"Error fetching history: {e.response['error']}")
         return []
+
+
+async def get_bot_id(client) -> str:
+    """
+    Retrieves the bot ID using the provided Slack WebClient.
+
+    Returns:
+        str: The bot ID.
+    """
+    try:
+        response = client.auth_test()
+        return response["user_id"]
+    except SlackApiError as e:
+        print(f"Error fetching bot ID: {e.response['error']}")
+        return 'None'
