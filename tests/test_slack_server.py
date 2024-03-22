edf9ee1dcc1eaae26e19f0882bd4048291984acc
Bryce York <bryce@bryceyork.com>
2024-03-22 19:55:17 -0400

    test: fix test suite stub for slack_server to mock the Slack WebClient

--- tests/test_slack_server.py
+++ tests/test_slack_server.py
@@ -1,5 +1,20 @@
-from hackathon_2023 import slack_server
+import pytest
+
+from unittest.mock import patch, MagicMock
 
 
-def test_pass():
+@pytest.fixture
+def mock_dependencies():
+    with patch('slack_sdk.WebClient', new_callable=MagicMock) as mock_WebClient:
+        mock_WebClient.return_value.chat_postMessage.return_value = {"ok": True}
+        yield mock_WebClient
+
+
+@pytest.fixture
+def slack_server(mock_dependencies):
+    from hackathon_2023 import slack_server
+    return slack_server
+
+
+def test_pass(slack_server, mock_dependencies):
     assert True
