f6223ee55833f296c49b1dfa5a1bfdbedee80c38
Bryce York <bryce@bryceyork.com>
2024-08-27 20:49:03 -0700

    test: resolve issues with the decorator test suite

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,5 +1,6 @@
 import pytest
 from unittest.mock import AsyncMock, MagicMock, patch
+from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 from ossai.decorators import safe_slack_api_call
 
@@ -7,13 +8,13 @@ from ossai.decorators import safe_slack_api_call
 @pytest.mark.asyncio
 async def test_safe_slack_api_call_happy_path():
     # Setup
-    client = MagicMock()
+    client = AsyncMock(spec=WebClient)
     mock_func = AsyncMock()
     mock_func.return_value = "Success"
     decorated_func = safe_slack_api_call(mock_func)
 
     # Create a mock payload with channel_id and a mock ack function
-    mock_payload = {"channel_id": "C123"}
+    mock_payload = {"channel_id": "C123", "user_id": "U123"}
     mock_ack = AsyncMock()
 
     # Execute
@@ -34,15 +35,17 @@ async def test_safe_slack_api_call_happy_path():
 @patch("ossai.decorators.logger")
 async def test_safe_slack_api_call_error_handling(mock_logger):
     # Setup
-    client = MagicMock()
+    client = AsyncMock(spec=WebClient)
     mock_func = AsyncMock()
     mock_func.side_effect = SlackApiError(
         message="Pineapple on pizza error", response={"error": "API error"}
     )
     decorated_func = safe_slack_api_call(mock_func)
 
+    client.chat_postEphemeral = AsyncMock()
+
     # Create a mock payload with channel_id and a mock ack function
-    mock_payload = {"channel_id": "C123"}
+    mock_payload = {"channel_id": "C123", "user_id": "U123"}
     mock_ack = AsyncMock()
 
     # Execute
